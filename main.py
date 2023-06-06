0c8d5acc896956ce782023c246d4f6a5d70ff7ca
CandiceZFish-XYZ <23563276+CandiceZFish-XYZ@users.noreply.github.com>
2023-06-06 22:15:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+def exercise1() :
+  print("hello")
+
+def exercise2() :
+  name = input("What is your name?")
+  print("hello {}".format(name))
+
+if __name__ == "__main__":
+      # exercise1()
+    exercise2()
