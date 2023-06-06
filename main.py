d8d54cec5fd2657fdf475c346429e6c309303fcb
CandiceZFish-XYZ <23563276+CandiceZFish-XYZ@users.noreply.github.com>
2023-06-06 22:34:30 +0000

    delete exercise 2 code

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ def exercise1() :
   print("hello")
 
 def exercise2() :
-  name = input("What is your name?")
-  print("hello {}".format(name))
+  
 
 if __name__ == "__main__":
       # exercise1()
